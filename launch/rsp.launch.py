f4f7e3795155f28d3271605ef52b154792e26c1f
KevinKipkorir254 <kipkorir.magut254@gmail.com>
2023-09-24 10:54:02 +0300

    In this I create a hardware interface yaml that uses the one supplied by the examples in the demo so that now I can write a node that just controls them, this is just awesome

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,9 +3,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration,  Command, PathJoinSubstitution, FindExecutable
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 import xacro
 
@@ -22,6 +23,43 @@ def generate_launch_description():
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+
+
+     
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("new_bot"),
+                    "description",
+                    "robot.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("ros2_control_demo_example_1"),
+            "config",
+            "rrbot_controllers.yaml",
+        ]
+    )
+
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        output="both",
+    )
+
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -29,6 +67,9 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # This causes the robot to appear
+    rviz = Node(package='rviz2', executable='rviz2',)
+    jspg = Node(package = 'joint_state_publisher', executable = 'joint_state_publisher')
 
     # Launch!
     return LaunchDescription([
@@ -37,5 +78,8 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        rviz,
+        jspg,
+        control_node,
     ])
